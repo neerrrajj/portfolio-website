640784376db9fa2c5d81fe3a915c7e66ce891ed5
Neeraj <nxxwxj@gmail.com>
2022-02-06 17:29:50 +0530

    added bare About container and completed Heading component

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,7 +1,15 @@
 import './Heading.css'
 
-const Heading = () => {
-  return <div>Heading</div>;
-};
+const Heading = props => {
+  return (
+    <div>
+      <p className="subtext">{props.sub}</p>
+      <div className="app__heading-main">
+        <p>{props.title}</p>
+        <div className='app__heading-main-line' />
+      </div>
+    </div>
+  )
+}
 
-export default Heading;
+export default Heading
--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -1,7 +1,34 @@
+import {Heading} from '../../components/'
+
 import './About.css'
 
 const About = () => {
-  return <div>About</div>;
-};
+  return (
+    <div className="app__about">
+      <div className="app__about-content">
+        <Heading sub="Who am I?" title="About me" />
+        <p>
+          Hello! My name is Neeraj and I love designing and creating stuff. My
+          fascination with websites began many years ago when I imagined how
+          wonderful it would be to create helpful websites and publish them on
+          the internet for everyone to use.
+        </p>
+        <p>
+          And here I am today, leveraging the power of code to create
+          user-centric, aesthetically-pleasing design. My major goal right now
+          is to join a startup that aligns with my vision and help them in
+          reaching greater heights.
+        </p>
+        <p>Here are a few technologies I'm familiar with:</p>
+        <ul>
+          <li>Languages : HTML, CSS, JavaScript (ES6+)</li>
+          <li>Frameworks / Libraries : React, Material UI</li>
+          <li>Design : Figma (wireframing, prototyping)</li>
+          <li>Tools : Git & Github</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
-export default About;
+export default About
