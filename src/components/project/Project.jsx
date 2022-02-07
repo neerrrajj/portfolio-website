4de09bc5a4fb4a3350f230a26d0f6e4d7b3f3ccc
Neeraj <nxxwxj@gmail.com>
2022-02-07 13:50:08 +0530

    incomplete Projects container

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,7 +1,13 @@
 import './Project.css'
 
-const Project = () => {
-  return <div>Project</div>;
-};
+const Project = props => {
+  return (
+    <div className="app__project">
+      <img src={props.url} alt='project' />
+      <p>{props.url}</p>
+      <p>{props.title}</p>
+    </div>
+  )
+}
 
-export default Project;
+export default Project
--- src/container/projects/Projects.jsx
+++ src/container/projects/Projects.jsx
@@ -1,7 +1,62 @@
+import { Heading } from '../../components'
+import { Project } from '../../components'
+
 import './Projects.css'
 
-const Projects = () => {
-  return <div>Projects</div>;
-};
+const figmaDesigns = [
+  {
+    url: '../../assets/figmadesign.png',
+    title: 'Admin Dashboard',
+    id: 1
+  },
+  {
+    url: '../../assets/figmadesign.png',
+    title: 'Landing page',
+    id: 2
+  },
+  {
+    url: '../../assets/figmadesign.png',
+    title: 'Graphic Design studio website',
+    id: 3
+  },
+]
 
-export default Projects;
+const figmaToReact = [
+  {
+    url: '../../assets/figmadesign.png',
+    title: 'Portfolio website',
+    id: 4
+  },
+  {
+    url: '../../assets/figmadesign.png',
+    title: 'AI Startup website',
+    id: 5
+  },
+  {
+    url: '../../assets/figmadesign.png',
+    title: 'Restaurant website',
+    id: 6
+  },
+]
+
+const Projects = () => {
+  return (
+    <div className="app__projects">
+      <Heading sub="What have I done?" title="Projects" />
+      <p className="headings">Figma Designs</p>
+      <div className="projects">
+        {figmaDesigns.map(project => {
+          return <Project title={project.title} url={project.url} key={project.id} />
+        })}
+      </div>
+      <p className="headings">Figma to React</p>
+      <div className="projects">
+        {figmaToReact.map(project => {
+          return <Project title={project.title} url={project.url} key={project.id} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Projects
