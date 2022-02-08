c79ae264484c10e9dd6c4892a1439853f9aa1ca0
Neeraj <nxxwxj@gmail.com>
2022-02-09 00:37:32 +0530

    established and displayed the contents of Modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,20 @@
+import { Heading } from '../'
 import './Modal.css'
+import figma1 from '../../assets/figma1.png'
 
-const Modal = () => {
-  return <div>Modal</div>;
-};
+const Modal = props => {
+  return (
+    <div className="modal">
+      <div className="backdrop" onClick={props.onClose} />
+      <div className="modal__overlay">
+        <Heading sub="Figma Designs" title={props.title} />
+        <div className="modal__overlay-img">
+          <img src={figma1} />
+          {/* <img src={props.url} /> */}
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Modal;
+export default Modal
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,16 +1,41 @@
+import { Fragment, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+import { Modal } from '../'
 import './Project.css'
 
 const Project = props => {
+  const [showModal, setShowModal] = useState(false)
+
+  const showModalHandler = () => {
+    setShowModal(true)
+  }
+
+  const closeModalHandler = () => {
+    setShowModal(false)
+  }
+
   return (
-    <div className="app__project">
-      <div className="app__project-img">
-        <img src={props.url} alt="project" />
+    <Fragment>
+      <div className="app__project" onClick={showModalHandler}>
+        <div className="app__project-img">
+          <img src={props.url} alt="project" />
+        </div>
+        <div className="app__project-title">
+          <p>{props.title}</p>
+          <p>❯</p>
+        </div>
       </div>
-      <div className="app__project-title">
-        <p>{props.title}</p>
-        <p>❯</p>
-      </div>
-    </div>
+      {showModal &&
+        ReactDOM.createPortal(
+          <Modal
+            onClose={closeModalHandler}
+            title={props.title}
+            url={props.url}
+          />,
+          document.getElementById('modal')
+        )}
+    </Fragment>
   )
 }
 
